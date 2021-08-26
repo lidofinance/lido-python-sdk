3068e19c03f4a8771ab2d771f1e2fd06db348a09
Raman <roman.s@p2p.org>
2021-08-26 16:35:04 +0300

    add stats methods and fix tests coverage

---
+++ lido/methods/stats.py
@@ -0,0 +1,17 @@
+from web3 import Web3
+
+from lido.contract import LidoContract
+
+
+def get_stats(w3: Web3):
+    return {
+        "isStopped": LidoContract.isStopped(w3)[""],
+        "totalPooledEther": LidoContract.getTotalPooledEther(w3)[""],
+        "withdrawalCredentials": LidoContract.getWithdrawalCredentials(w3)[""],
+        "bufferedEther": LidoContract.getBufferedEther(w3)[""],
+        **LidoContract.getFee(w3),
+        **LidoContract.getFeeDistribution(w3),
+        **LidoContract.getBeaconStat(w3),
+        "last_block": w3.eth.getBlock("latest")["number"],
+        "last_blocktime": w3.eth.getBlock("latest")["timestamp"],
+    }
