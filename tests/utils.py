f4dd69605040a6b50be926dd00b4082bfd864a49
F4ever <F4ever@users.noreply.github.com>
2021-08-30 19:46:15 +0000

    :art: Format Python code with psf/black

--- tests/utils.py
+++ tests/utils.py
@@ -17,6 +17,10 @@ def get_mainnet_provider():
 
 
 def _get_web3_provider(net: str):
-    w3 = Web3(Web3.HTTPProvider(f"https://{net}.infura.io/v3/b11919ed73094499a35d1b3fa338322a"))
+    w3 = Web3(
+        Web3.HTTPProvider(
+            f"https://{net}.infura.io/v3/b11919ed73094499a35d1b3fa338322a"
+        )
+    )
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     return w3
