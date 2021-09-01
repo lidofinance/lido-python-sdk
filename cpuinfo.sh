e920f5f50cb428186d9121d9c7bbd3b2485a67f5
infloop <infloop.gm@gmail.com>
2021-09-01 17:01:50 +0300

    refactor: added cpuinfo script, changed publish script

---
+++ cpuinfo.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+if (grep -q -e '^flags.*\badx\b' /proc/cpuinfo) 2>/dev/null; then
+    echo "[present] adx"
+else
+    echo "[absent] adx"
+fi
+
+if (grep -q -e '^flags.*\bavx\b' /proc/cpuinfo) 2>/dev/null; then
+    echo "[present] avx"
+else
+    echo "[absent] avx"
+fi
+
+if (grep -q -e '^flags.*\bavx2\b' /proc/cpuinfo) 2>/dev/null; then
+    echo "[present] avx2"
+else
+    echo "[absent] avx2"
+fi
