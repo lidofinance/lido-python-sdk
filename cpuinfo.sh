cd336fb6f90be91350a1e857e5de39739cc01708
infloop <infloop.gm@gmail.com>
2021-09-01 17:31:34 +0300

    refactor: force skipped adx instructions

--- cpuinfo.sh
+++ cpuinfo.sh
@@ -5,13 +5,11 @@ if (grep -q -e '^flags.*\badx\b' /proc/cpuinfo) 2>/dev/null; then
 else
     echo "[absent] adx"
 fi
-
 if (grep -q -e '^flags.*\bavx\b' /proc/cpuinfo) 2>/dev/null; then
     echo "[present] avx"
 else
     echo "[absent] avx"
 fi
-
 if (grep -q -e '^flags.*\bavx2\b' /proc/cpuinfo) 2>/dev/null; then
     echo "[present] avx2"
 else
