e4e4fc571c3d7d2a2ff0fcd7e9425c5ab52e43c2
Raman <roman.s@p2p.org>
2021-09-09 16:09:15 +0300

    increase version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ URL = "https://github.com/lidofinance/lido-python-sdk"
 EMAIL = "info@lido.fi"
 AUTHOR = "Lido"
 REQUIRES_PYTHON = ">=3.7,<4"
-VERSION = "2.1.1"
+VERSION = "2.1.2"
 
 
 # C/C++ Extensions
