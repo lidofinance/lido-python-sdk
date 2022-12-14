9a25197cb2a881d3b5ff9ae9f8a01b9a2daa0f1c
Raman Siamionau <raman.s@lido.fi>
2022-12-14 10:29:58 +0100

    pypi tests

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
 # Where the magic happens:
 setup(
     name="lido-sdk",
-    version="2.6.0",
+    version="2.6.5",
     description="This library consolidates various functions to efficiently load network data for Lido,"
                 " validate node  operator keys and find key duplicates.",
     long_description=long_description,
